c9c0490d4ea5c92d214e073b76dab0058d1b19f8
aninima01 <animabhattacharjee898@gmail.com>
2024-08-13 15:59:03 +0530

    added auth files and auth tests

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -25,6 +25,7 @@ export const AppContextProvider = ({
   const { isError } = useQuery("validateToken", apiClient.validateToken, {
     retry: false,
   });
+
   return (
     <AppContext.Provider
       value={{
